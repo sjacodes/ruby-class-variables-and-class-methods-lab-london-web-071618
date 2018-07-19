c1a37b9cf847e9e7b5e7d03f8d13f480920e0f36
Sarah Jacob <smjacob91@gmail.com>
2018-07-19 09:13:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,34 @@
 class Song
-  @@count = []
+  @@count = 0
+  @@genres = []
+  @@artists = []
   
   attr_accessor :name, :artist, :genre
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    @@genre_count = {}
   end
   
+  def self.count
+    @@count
+  end
+    
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+    
 end
