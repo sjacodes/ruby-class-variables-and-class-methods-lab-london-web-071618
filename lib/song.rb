c9c1ca4f250a36762e179e6fce2060010cfd7551
Sarah Jacob <smjacob91@gmail.com>
2018-07-19 11:07:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   @@count = 0
   @@genres = []
@@ -12,7 +14,7 @@ class Song
     @@count += 1
     @@genres << genre
     @@artists << artist
-    @@genre_count = {}
+    
   end
   
   def self.count
@@ -28,7 +30,30 @@ class Song
   end
   
   def self.genre_count
-    @@genre_count
+    genre_count = {}
+    
+    
+    
+    
+    
+    
+    
+    if @genre_count.include?
+      
+      
+      
+      
+    if @genre_count[genre] == nil
+      @genre_count[genre] = 0
+      @genre_count[genre] = genre_count[genre] + 1
+    else
+      return genre_count[genre]
+    end
   end
     
 end
+
+
+#creating a hash called genre count which is initally empty
+#if the hash has the genre key, dont re-add the key, just add the the value
+#if the hash does not have the genre key already existing, add the genre key, along with the value associated with with( ie 1)
